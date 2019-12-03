777425b7ef45883e96372a99d5423e329b0f9720
Wong Kah Wo <s10189113@connect.np.edu.sg>
2019-12-03 11:59:49 +0800

    problems: assistant view controller cannot connect to swift file

---
+++ MAD2_Practical6/QRViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QRViewController.swift
+//  MAD2_Practical6
+//
+//  Created by MAD2_P02 on 3/12/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import QRCodeReader
+
+class QRViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
