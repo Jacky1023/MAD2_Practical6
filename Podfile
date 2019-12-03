ba822d4a28a369a05dd4cbb38b641f1d3ca78185
Wong Kah Wo <s10189113@connect.np.edu.sg>
2019-12-03 09:36:42 +0800

    create one tab bar , two view controller

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'MAD2_Practical6' do
+  pod 'QRCodeReader.swift', '~> 10.1'
+end
---
+++ Pods/Target Support Files/Pods-MAD2_Practical6/Pods-MAD2_Practical6-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MAD2_Practical6 : NSObject
+@end
+@implementation PodsDummy_Pods_MAD2_Practical6
+@end
